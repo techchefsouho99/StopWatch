37dafbfc918882fd323b894f2b24c030f5942ced
SOUHARDYA ADHIKARY <souhardyaadhikary86942@gmail.com>
2019-06-05 21:33:35 +0530

    start button added

--- stopwatch.py
+++ stopwatch.py
@@ -1,4 +1,13 @@
 import tkinter as Tkinter
-
+def start(label):
+    global running
+    running=True
+    start['state']='disabled'
 root=Tkinter.Tk()
 root.title("STOPWATCH")
+root.minsize(width=250,height=70)
+label=Tkinter.Label(root,text="WELCOME!",fg="black",font="Verdana 30 bold")
+label.pack()
+start=Tkinter.Button(root,text="START",width="15",command=lambda:start(label))
+start.pack()
+root.mainloop()
