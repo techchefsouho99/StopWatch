dbf07bb6334a81eb23bf96cfd145c3be8456d086
SOUHARDYA ADHIKARY <souhardyaadhikary86942@gmail.com>
2019-06-05 21:56:22 +0530

    counter function added

--- stopwatch.py
+++ stopwatch.py
@@ -1,13 +1,35 @@
 import tkinter as Tkinter
-def start(label):
+
+counter =-1
+runnning=False
+
+def counter_label(label):
+    def count():
+        if running:
+            global counter
+            if counter==-1:
+                display="STARTING..."
+            else:
+                display=str(counter)
+            label['text']=display
+            label.after(1000,count)
+            counter+=1
+    count()
+
+
+def Start(label):
     global running
     running=True
+    counter_label(label)
     start['state']='disabled'
+
+
 root=Tkinter.Tk()
 root.title("STOPWATCH")
 root.minsize(width=250,height=70)
 label=Tkinter.Label(root,text="WELCOME!",fg="black",font="Verdana 30 bold")
 label.pack()
-start=Tkinter.Button(root,text="START",width="15",command=lambda:start(label))
+start=Tkinter.Button(root,text='START',width=15,command=lambda:Start(label))
 start.pack()
 root.mainloop()
+
